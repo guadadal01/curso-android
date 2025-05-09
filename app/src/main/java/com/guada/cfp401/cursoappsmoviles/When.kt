655a8ad8c503fda402ad06953e437d5ae0ac4f89
guada <guadalupedalfonso10@gmail.com>
2025-05-09 17:35:17 -0300

    avance con when

---
+++ app/src/main/java/com/guada/cfp401/cursoappsmoviles/When.kt
@@ -0,0 +1,14 @@
+package com.guada.cfp401.cursoappsmoviles
+
+
+fun main() {
+    getMonth(3)
+    getMonth (8)
+}
+fun getMonth (mes: Int){
+    when(mes) {
+    in 1..6-> println ("Primer semestre")
+    in 7..12-> println("Segundo semestre")
+        !in 1..12 -> println ("Ingrese un mes valido")
+      }
+}
