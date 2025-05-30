00a43d4bbba6ce4201396d93f60a1c1288be169e
guada <guadalupedalfonso10@gmail.com>
2025-05-30 17:16:53 -0300

    avance con arrays2

--- app/src/main/java/com/guada/cfp401/cursoappsmoviles/Ejercicios/EjArrays.kt
+++ app/src/main/java/com/guada/cfp401/cursoappsmoviles/Ejercicios/EjArrays.kt
@@ -14,8 +14,25 @@ fun ejArray2B(){
     val misNumeros: Array<Int> = arrayOf(3,6,8,9,1,2,4,7)
     var sumaTotal: Int = misNumeros.sum()
     println("La suma total es $sumaTotal")
-
+}
+fun ejArray3A (){
+    val numeros: Array<Int> = arrayOf(5,12,8,2,3,11,1,4,10,8,7,6,9)
+    var minimo: Int = Int.MAX_VALUE
+    var maximo: Int = Int.MIN_VALUE
+    for(num in numeros) {
+        if (num < minimo) minimo = num
+        if (num > maximo) maximo = num
+    }
+        println("El menor numero es $minimo")
+        println("El mayor numero es $maximo")
+}
+fun ejArray3B(){
+    val numeros: Array<Int> = arrayOf(5,12,8,2,3,11,1,4,10,8,7,6,9)
+    var minimo: Int = numeros.min()
+    var maximo: Int = numeros.max()
+    println ("El minimo numero es: $minimo")
+    println ("El maximo numero es: $maximo")
 }
 fun main (){
-    ejArray2A()
+    ejArray3B()
 }
